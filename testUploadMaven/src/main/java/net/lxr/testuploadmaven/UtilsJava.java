aeba2c68f8b959ce88ec94b5f8aac96d968e2a52
lxr17 <lanxingren17@gmail.com>
2023-05-30 16:50:12 +0800

    测试新加一个类

---
+++ testUploadMaven/src/main/java/net/lxr/testuploadmaven/UtilsJava.java
@@ -0,0 +1,11 @@
+package net.lxr.testuploadmaven;
+
+public class UtilsJava {
+    public static int getStringLength(String str) {
+        if (str == null) {
+            return 0;
+        }
+
+        return str.length();
+    }
+}
